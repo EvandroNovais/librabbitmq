f7a9dc39872cbe4220265d32eaddb528bd6eeb5d
EvandroNovais <evandro-novais@outlook.com>
2021-09-07 20:59:19 -0300

    Initial Commit

---
+++ src/main/java/dtos/PriceDto.java
@@ -0,0 +1,9 @@
+package dtos;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class PriceDto implements Serializable {
+    public String idProduct;
+    public BigDecimal price;
+}
---
+++ src/main/java/dtos/StockDto.java
@@ -0,0 +1,8 @@
+ package dtos;
+
+import java.io.Serializable;
+
+public class StockDto implements Serializable {
+     public String idProduct;
+     public int quantity;
+}
